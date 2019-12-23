238b95f7a3e39206ae46128ae09fc0d37741fde8
al3xrz <al3xrz@gmail.com>
2019-12-23 16:52:50 +0300

    tooltips fot markers

---
+++ icons.js
@@ -0,0 +1,21 @@
+green_icon = L.icon({
+    iconUrl: 'icons/green.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+})
+
+
+
+
+
+red_icon = L.icon({
+    iconUrl: 'icons/red.png',
+    iconSize: [21, 40],
+})
+
+yellow_icon = L.icon({
+    iconUrl: 'icons/yellow.png',
+    iconSize: [21, 40],
+})
---
+++ ll.js
@@ -0,0 +1,32 @@
+var map = L.map('map', {
+    center: [42.961903,47.549033],
+    zoom: 13
+});
+var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(osm);
+var layerGroup = L.layerGroup().addTo(map);
+
+green_icon_tooltip = L.tooltip({
+    direction : 'left',
+    permanent : true,
+    //offset: L.point(-17, -35),
+    offset: L.point(0, 0),
+    opacity: 1,
+    className: 'tt-css',
+})
+
+
+
+
+
+
+
+marker = L.marker([42.959350,47.552572], {  icon: green_icon } ).bindPopup('text marker 1');
+marker.bindTooltip(green_icon_tooltip);
+marker.setTooltipContent('<img src="icons/net.png" width=20/><img src="icons/220.png" width=20/>');
+//marker.unbindTooltip()
+
+
+
+
+marker.addTo(layerGroup)
